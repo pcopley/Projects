e50626f22ec17db91adaa29543a3917a36475968
Phillip Copley <phillip.copley@gmail.com>
2013-08-07 08:00:55 -0400

    Remove commented code

--- 005_FlooringCost/costcalc.erl
+++ 005_FlooringCost/costcalc.erl
@@ -5,16 +5,6 @@
 %% API
 -export([start/0]).
 
-%%docalc() ->
-%%  io:fwrite("Please terminate each expression with a period ('.').\n"),
-%%  ReadWidth = io:get_line("Width: "),
-%%  ReadHeight = io:get_line("Height: "),
-%%  ReadCost = io:get_line("Cost: $"),
-%%  {_,Width} = ReadWidth,
-%%  {_,Height} = ReadHeight,
-%%  {_,Cost} = ReadCost,
-%%  io:fwrite(Width + " * " + Height + " * " + Cost + " = " + Width * Height * Cost).
-
 start() ->
   io:fwrite("Calculate the cost of flooring based on width, height, and cost per square foot.\n"),
   {W,_} = string:to_integer(io:get_line("Width in feet: ")),
